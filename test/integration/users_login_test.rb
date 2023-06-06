1718a3a8a317d13a174648635ec573b3917918ad
Viktoriia <viktoriia@zenbook.home>
2023-06-06 18:33:52 +0200

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,6 +18,7 @@ class InvalidPasswordTest <UsersLogin
     post login_path, params: { session: { email:    @user.email,
                                password: "invalid" } }
     assert_not is_logged_in?
+    assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -42,6 +43,7 @@ class ValidLoginTest < ValidLogin
 
   test "redirect after login" do
     follow_redirect!
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
@@ -70,4 +72,26 @@ class LogoutTest < Logout
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
 end
+
+class RememberingTest <  UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
+
