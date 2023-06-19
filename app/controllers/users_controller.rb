83c1ddd948b7ab4cf7c8fc27fe957b44ea207a47
Viktoriia <viktoriia@zenbook.home>
2023-06-19 14:35:13 +0200

    Add password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
     redirect_to users_url, status: :see_other
   end
 
-private
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, 
