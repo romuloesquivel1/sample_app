6d57d0bcd40ab18e569ec66ece9b5528832de9a2
Viktoriia <viktoriia@zenbook.home>
2023-06-26 12:26:08 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -75,6 +76,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
 
   # Converts email to all lowercase.
