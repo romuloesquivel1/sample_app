853e59c17b1c8c7f1188bf067d2f8553076f5b3d
Viktoriia <viktoriia@zenbook.home>
2023-06-30 12:59:27 +0200

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,11 @@ users = User.order(:created_at).take(6)
   content = Faker::ChuckNorris.fact
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+#Create following reletionships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
